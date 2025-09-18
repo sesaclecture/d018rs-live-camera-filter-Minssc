8ff81167e0b301543849b3700449d63bafde483b
Minsung <pauaup@naver.com>
2025-09-18 16:47:09 +0900

    initial commit

--- src/filters.py
+++ src/filters.py
@@ -4,25 +4,42 @@ import numpy as np
 
 class Filters:
     # TODO: Image kernels
-    Kernels = {
+    Kernels:dict = {
+        'Original': np.array([[0, 0, 0], [0, 1, 0], [0, 0 ,0]], dtype=np.float32),
+        'Blur' : np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]], dtype=np.float32) / 9,
+        'Gaussian blur' : np.array([[1/16, 2/16, 1/16], [2/16, 4/16, 2/16], [1/16, 2/16, 1/16]], dtype=np.float32),
+        'Sharpen': np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]], dtype=np.float32),
+        'Sobel (X)': np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], dtype=np.float32),
+        'Sobel (Y)': np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]], dtype=np.float32),
+        'Edge detection': np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]], dtype=np.float32),
+        'Emboss': np.array([[-2, -1, 0], [-1, 1, 1], [0, 1, 2]], dtype=np.float32)
     }
 
-    def __init__(self, kernels=Kernels):
+    def __init__(self, kernels:dict=Kernels):
         self.kernels = kernels
-        # TODO: Implement internal variables
+        self.current_kernel = next(iter(kernels.keys()))
 
     def apply_filter(self, frame, filter_name) -> np.array:
-        # TODO: Apply the selected filter kernel to the frame
-        pass
+        self.current_kernel = filter_name
+        return cv2.filter2D(frame, -1, self.kernels[self.current_kernel])
 
     def get_current_filter_name(self) -> str:
-        # TODO: Return currently set kernels's name
-        pass
+        return self.current_kernel
 
     def switch_next_filter(self):
-        # TODO: Update currently selected kernel to the next
-        pass
+        for i, k in enumerate(self.kernels.keys()):
+            if k == self.current_kernel:
+                break
+
+        next = list(self.kernels.keys())[(i + 1) % len(self.kernels)]
+        self.current_kernel = next
+        return self.current_kernel
 
     def switch_previous_filter(self):
-        # TODO: Update currently selected kernel to the previous
-        pass
+        for i, k in enumerate(self.kernels.keys()):
+            if k == self.current_kernel:
+                break
+
+        next = list(self.kernels.keys())[(i - 1 + len(self.kernels)) % len(self.kernels)]
+        self.current_kernel = next
+        return self.current_kernel
